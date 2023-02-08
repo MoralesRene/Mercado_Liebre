93296683f0e0f7475538619033f6980687903667
MoralesRene <renemoralesquispe@gmail.com>
2023-02-07 22:22:27 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const path = require("path")
+const app= express()
+const port =3030;
+const publicPath= path.resolve(__dirname,"./public")
+
+app.use(express.static(publicPath))
+app.listen(port, ()=>{
+    console.log(`Server Started http://localhost:${port}`);
+})
+app.get("/", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/index.html"))
+})
+app.get("/register", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+})
+app.get("/login", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
